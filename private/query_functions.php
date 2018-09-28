6ff3a61f399255fb099ae649c48a697b7ec4e959
ahmedkorim <ahmedkorrim@gmail.com>
2018-09-28 05:33:24 +0200

    pages guard

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,68 +1,77 @@
 <?php
 
-// Subjects
+  // Subjects
 
-function find_all_subjects() {
+  function find_all_subjects($options=[]) {
     global $db;
 
+    $visible = $options['visible'] ?? false;
+
     $sql = "SELECT * FROM subjects ";
+    if($visible) {
+      $sql .= "WHERE visible = true ";
+    }
     $sql .= "ORDER BY position ASC";
     //echo $sql;
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     return $result;
-}
+  }
 
-function find_subject_by_id($id) {
+  function find_subject_by_id($id, $options=[]) {
     global $db;
 
+    $visible = $options['visible'] ?? false;
+
     $sql = "SELECT * FROM subjects ";
-    $sql .= "WHERE id='" . db_escape($db, $id) . "'";
+    $sql .= "WHERE id='" . db_escape($db, $id) . "' ";
+    if($visible) {
+      $sql .= "AND visible = true";
+    }
     // echo $sql;
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     $subject = mysqli_fetch_assoc($result);
     mysqli_free_result($result);
     return $subject; // returns an assoc. array
-}
+  }
 
-
-function validate_subject($subject) {
+  function validate_subject($subject) {
     $errors = [];
 
     // menu_name
-    if (is_blank($subject['menu_name'])) {
-        $errors[] = "Name cannot be blank.";
-    } elseif (!has_length($subject['menu_name'], ['min' => 2, 'max' => 255])) {
-        $errors[] = "Name must be between 2 and 255 characters.";
+    if(is_blank($subject['menu_name'])) {
+      $errors[] = "Name cannot be blank.";
+    } elseif(!has_length($subject['menu_name'], ['min' => 2, 'max' => 255])) {
+      $errors[] = "Name must be between 2 and 255 characters.";
     }
 
     // position
     // Make sure we are working with an integer
-    $postion_int = (int)$subject['position'];
-    if ($postion_int <= 0) {
-        $errors[] = "Position must be greater than zero.";
+    $postion_int = (int) $subject['position'];
+    if($postion_int <= 0) {
+      $errors[] = "Position must be greater than zero.";
     }
-    if ($postion_int > 999) {
-        $errors[] = "Position must be less than 999.";
+    if($postion_int > 999) {
+      $errors[] = "Position must be less than 999.";
     }
 
     // visible
     // Make sure we are working with a string
-    $visible_str = (string)$subject['visible'];
-    if (!has_inclusion_of($visible_str, ["0", "1"])) {
-        $errors[] = "Visible must be true or false.";
+    $visible_str = (string) $subject['visible'];
+    if(!has_inclusion_of($visible_str, ["0","1"])) {
+      $errors[] = "Visible must be true or false.";
     }
 
     return $errors;
-}
+  }
 
-function insert_subject($subject) {
+  function insert_subject($subject) {
     global $db;
 
     $errors = validate_subject($subject);
-    if (!empty($errors)) {
-        return $errors;
+    if(!empty($errors)) {
+      return $errors;
     }
 
     $sql = "INSERT INTO subjects ";
@@ -74,22 +83,22 @@ function insert_subject($subject) {
     $sql .= ")";
     $result = mysqli_query($db, $sql);
     // For INSERT statements, $result is true/false
-    if ($result) {
-        return true;
+    if($result) {
+      return true;
     } else {
-        // INSERT failed
-        echo mysqli_error($db);
-        db_disconnect($db);
-        exit;
+      // INSERT failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
-}
+  }
 
-function update_subject($subject) {
+  function update_subject($subject) {
     global $db;
 
     $errors = validate_subject($subject);
-    if (!empty($errors)) {
-        return $errors;
+    if(!empty($errors)) {
+      return $errors;
     }
 
     $sql = "UPDATE subjects SET ";
@@ -101,18 +110,18 @@ function update_subject($subject) {
 
     $result = mysqli_query($db, $sql);
     // For UPDATE statements, $result is true/false
-    if ($result) {
-        return true;
+    if($result) {
+      return true;
     } else {
-        // UPDATE failed
-        echo mysqli_error($db);
-        db_disconnect($db);
-        exit;
+      // UPDATE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
 
-}
+  }
 
-function delete_subject($id) {
+  function delete_subject($id) {
     global $db;
 
     $sql = "DELETE FROM subjects ";
@@ -121,19 +130,19 @@ function delete_subject($id) {
     $result = mysqli_query($db, $sql);
 
     // For DELETE statements, $result is true/false
-    if ($result) {
-        return true;
+    if($result) {
+      return true;
     } else {
-        // DELETE failed
-        echo mysqli_error($db);
-        db_disconnect($db);
-        exit;
+      // DELETE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
-}
+  }
 
-// Pages
+  // Pages
 
-function find_all_pages() {
+  function find_all_pages() {
     global $db;
 
     $sql = "SELECT * FROM pages ";
@@ -141,85 +150,76 @@ function find_all_pages() {
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     return $result;
-}
+  }
 
-function find_page_by_id($id) {
+  function find_page_by_id($id, $options=[]) {
     global $db;
 
+    $visible = $options['visible'] ?? false;
+
     $sql = "SELECT * FROM pages ";
-    $sql .= "WHERE id='" . db_escape($db, $id) . "'";
+    $sql .= "WHERE id='" . db_escape($db, $id) . "' ";
+    if($visible) {
+      $sql .= "AND visible = true";
+    }
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     $page = mysqli_fetch_assoc($result);
     mysqli_free_result($result);
     return $page; // returns an assoc. array
-}
+  }
 
-// find pages by subject id
-
-function find_pages_by_subject_id($id) {
-    global $db;
-
-    $sql = "SELECT * FROM pages ";
-    $sql .= "WHERE subject_id='" . db_escape($db, $id) . "' ";
-    $sql .= "ORDER BY position ASC";
-    $result = mysqli_query($db, $sql);
-    confirm_result_set($result);
-    return $result; // returns an assoc. array
-}
-
-
-function validate_page($page) {
+  function validate_page($page) {
     $errors = [];
 
     // subject_id
-    if (is_blank($page['subject_id'])) {
-        $errors[] = "Subject cannot be blank.";
+    if(is_blank($page['subject_id'])) {
+      $errors[] = "Subject cannot be blank.";
     }
 
     // menu_name
-    if (is_blank($page['menu_name'])) {
-        $errors[] = "Name cannot be blank.";
-    } elseif (!has_length($page['menu_name'], ['min' => 2, 'max' => 255])) {
-        $errors[] = "Name must be between 2 and 255 characters.";
+    if(is_blank($page['menu_name'])) {
+      $errors[] = "Name cannot be blank.";
+    } elseif(!has_length($page['menu_name'], ['min' => 2, 'max' => 255])) {
+      $errors[] = "Name must be between 2 and 255 characters.";
     }
     $current_id = $page['id'] ?? '0';
-    if (!has_unique_page_menu_name($page['menu_name'], $current_id)) {
-        $errors[] = "Menu name must be unique.";
+    if(!has_unique_page_menu_name($page['menu_name'], $current_id)) {
+      $errors[] = "Menu name must be unique.";
     }
 
 
     // position
     // Make sure we are working with an integer
-    $postion_int = (int)$page['position'];
-    if ($postion_int <= 0) {
-        $errors[] = "Position must be greater than zero.";
+    $postion_int = (int) $page['position'];
+    if($postion_int <= 0) {
+      $errors[] = "Position must be greater than zero.";
     }
-    if ($postion_int > 999) {
-        $errors[] = "Position must be less than 999.";
+    if($postion_int > 999) {
+      $errors[] = "Position must be less than 999.";
     }
 
     // visible
     // Make sure we are working with a string
-    $visible_str = (string)$page['visible'];
-    if (!has_inclusion_of($visible_str, ["0", "1"])) {
-        $errors[] = "Visible must be true or false.";
+    $visible_str = (string) $page['visible'];
+    if(!has_inclusion_of($visible_str, ["0","1"])) {
+      $errors[] = "Visible must be true or false.";
     }
 
     // content
-    if (is_blank($page['content'])) {
-        $errors[] = "Content cannot be blank.";
+    if(is_blank($page['content'])) {
+      $errors[] = "Content cannot be blank.";
     }
 
     return $errors;
-}
+  }
 
-function insert_page($page) {
+  function insert_page($page) {
     global $db;
 
     $errors = validate_page($page);
-    if (!empty($errors)) {
-        return $errors;
+    if(!empty($errors)) {
+      return $errors;
     }
 
     $sql = "INSERT INTO pages ";
@@ -233,22 +233,22 @@ function insert_page($page) {
     $sql .= ")";
     $result = mysqli_query($db, $sql);
     // For INSERT statements, $result is true/false
-    if ($result) {
-        return true;
+    if($result) {
+      return true;
     } else {
-        // INSERT failed
-        echo mysqli_error($db);
-        db_disconnect($db);
-        exit;
+      // INSERT failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
-}
+  }
 
-function update_page($page) {
+  function update_page($page) {
     global $db;
 
     $errors = validate_page($page);
-    if (!empty($errors)) {
-        return $errors;
+    if(!empty($errors)) {
+      return $errors;
     }
 
     $sql = "UPDATE pages SET ";
@@ -262,18 +262,18 @@ function update_page($page) {
 
     $result = mysqli_query($db, $sql);
     // For UPDATE statements, $result is true/false
-    if ($result) {
-        return true;
+    if($result) {
+      return true;
     } else {
-        // UPDATE failed
-        echo mysqli_error($db);
-        db_disconnect($db);
-        exit;
+      // UPDATE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
 
-}
+  }
 
-function delete_page($id) {
+  function delete_page($id) {
     global $db;
 
     $sql = "DELETE FROM pages ";
@@ -282,14 +282,31 @@ function delete_page($id) {
     $result = mysqli_query($db, $sql);
 
     // For DELETE statements, $result is true/false
-    if ($result) {
-        return true;
+    if($result) {
+      return true;
     } else {
-        // DELETE failed
-        echo mysqli_error($db);
-        db_disconnect($db);
-        exit;
+      // DELETE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
     }
-}
+  }
+
+  function find_pages_by_subject_id($subject_id, $options=[]) {
+    global $db;
+
+    $visible = $options['visible'] ?? false;
+
+    $sql = "SELECT * FROM pages ";
+    $sql .= "WHERE subject_id='" . db_escape($db, $subject_id) . "' ";
+    if($visible) {
+      $sql .= "AND visible = true ";
+    }
+    $sql .= "ORDER BY position ASC";
+    $result = mysqli_query($db, $sql);
+    confirm_result_set($result);
+    return $result;
+  }
+
 
 ?>
